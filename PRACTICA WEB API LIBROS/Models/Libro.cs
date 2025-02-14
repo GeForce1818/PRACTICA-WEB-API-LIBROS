12daac654e2afa5a9b9135b82d8e115a4cf1530a
AlexLC96 <soyunfurryowo@gmail.com>
2025-02-13 19:52:37 -0600

    adicion de clase libro

---
+++ PRACTICA WEB API LIBROS/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿namespace PRACTICA_WEB_API_LIBROS.Models
+{
+    public class Libro
+    {
+        public int id_libro { get; set; }
+        public string titulo { get; set; }
+        public int año_publicacion { get; set; }
+        public int autor_id { get; set; }
+        public int categoria_id { get; set; }
+        public string resumen { get; set; }
+
+
+
+    }
+}
