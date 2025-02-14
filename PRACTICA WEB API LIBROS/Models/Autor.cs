93b83cc4ed93888a616fbb05a0bed55a314e2381
AlexLC96 <soyunfurryowo@gmail.com>
2025-02-13 19:55:29 -0600

    adidion de clase autor

---
+++ PRACTICA WEB API LIBROS/Models/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace PRACTICA_WEB_API_LIBROS.Models
+{
+    public class Autor
+    {
+        public int id_autor { get; set; }
+        public string nombre { get; set; }
+        public string nacionalidad { get; set; }
+    }
+}
