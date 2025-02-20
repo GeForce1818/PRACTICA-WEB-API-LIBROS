3f72a0190b2c8d7619e9cd03c2c0271e2a23c452
AlexLC96 <soyunfurryowo@gmail.com>
2025-02-20 09:00:07 -0600

    Adición de controladores para Libro y Autor e inyección por dependencia

---
+++ PRACTICA WEB API LIBROS/Controllers/AutorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRACTICA_WEB_API_LIBROS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRACTICA_WEB_API_LIBROS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly BibliotecaContext _BibliotecaContexto;
+
+        public AutorController(BibliotecaContext BibliotecaContexto)
+        {
+            _BibliotecaContexto = BibliotecaContexto;
+        }
+    }
+}
---
+++ PRACTICA WEB API LIBROS/Controllers/LibroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRACTICA_WEB_API_LIBROS.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace PRACTICA_WEB_API_LIBROS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly BibliotecaContext _BibliotecaContexto;
+
+        public LibroController(BibliotecaContext BibliotecaContexto)
+        {
+            _BibliotecaContexto = BibliotecaContexto;
+        }
+
+    }
+}
